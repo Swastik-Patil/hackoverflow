629a3e3602e6dd7e9125974ce701f942031da5d6
Swastik-Patil <swastik592@gmail.com>
2023-03-16 19:13:31 +0530

    Fixed Erros

--- App.js
+++ App.js
@@ -1,4 +1,36 @@
-import { Chatty } from "react-native-chatty";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import LoginScreen, { SocialButton } from "react-native-login-screen";
 
+export default function App() {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
-<Chatty messages={messages} headerProps={''} footerProps={''} />
+  return (
+    <View style={styles.container}>
+      <LoginScreen
+        className="bg-white"
+        logoImageSource={require("./assets/social/logo.png")}
+        onLoginPress={() => {}}
+        onSignupPress={() => {}}
+        onEmailChange={() => {}}
+        onPasswordChange={() => {}}
+      >
+        <SocialButton
+          text="Continue with Google"
+          imageSource={require("./assets/social/Google.png")}
+          onPress={() => {}}
+        />
+      </LoginScreen>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
